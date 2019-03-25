ca49d4f16a9b8c2150e15fd8e8f8449dbef32086
Stefano Altamura <stephano288@gmail.com>
2019-03-25 09:24:40 +0100

    added folder in extras to help for containers

---
+++ extra/SimpleDockerCreation/CreateContainer.py
@@ -0,0 +1,10 @@
+# leggo json config che contiene la lista delle istanze -() formato: nome servizio + connection string) + range di porte da usare + range di porte usato
+# verifico che esista o meno la cartella con il nome del servizio
+# inserisco / creo i file Json di configurazione
+# creo docker con i vari parametri (porta nuova ecc.)
+# file di config: /etc/sqlexporter/sqlexporter.json
+# aggiungo config al file di prometheus
+
+import json
+
+
